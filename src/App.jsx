79f56571424a48b565ca8b6deef6ddf56cef1c14
au-lex <aulexc4d2@gmail.com>
2023-10-02 18:31:32 +0100

    op

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 
 import './App.css'
 import Hero from './Component/Hero'
-// import ScreenRecorder from "./ScreenRecorder"
-// import Fetch from './Component/Fetch'
+
 
 function App() {
 
 
   return (
     <>
-    {/* <ScreenRecorder /> */}
+   
     <Hero />
     {/* <Fetch /> */}
 
